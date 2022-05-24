cd39de986119090dcac616ce8e04e9ccf2a3ee67
ndtrang121 <ndtrang129@gmail.com>
2022-05-24 16:30:52 +0700

    async-await

---
+++ 9-async-await.js
@@ -0,0 +1,24 @@
+// Async function alway return Promise
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(a < 0 || b < 0){
+                return reject('Number must be positive')
+            }
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+const doWork = async () => {
+    const sum = await add(1, -99)
+    const sum2 = await add(sum, 50)
+    const sum3 = await add(sum2, 5)
+    return sum3
+}
+
+doWork().then((result) => {
+    console.log(result)
+}).catch((error) => {
+    console.log(error)
+})
